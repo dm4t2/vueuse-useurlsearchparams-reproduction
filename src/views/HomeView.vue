d6839e324c9089bb4c0f03acf8fb2901e1d75d23
dm4t2 <dm4t2@outlook.com>
2022-10-07 15:30:26 +0200

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div>Param "tab" is {{ tab }}</div>
+    <button @click="tab = 'foo'">Set param to 'foo'</button>
+  </div>
+</template>
+
+<script>
+import { toRefs } from '@vue/composition-api'
+import { useUrlSearchParams } from '@vueuse/core';
+
+export default {
+  name: 'HomeView',
+  setup() {
+    const params = useUrlSearchParams('history', {
+      initialValue: { tab: 'baz' }
+    });
+
+    return toRefs(params)
+  }
+}
+</script>
